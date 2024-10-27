fb86f5dcaf4963246158221573d6970e214d4918
PopCornIgm <popcornigorek.1993@gmail.com>
2024-10-27 21:09:17 +0700

    Application layout

---
+++ my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,7 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return <input type='text' className='form-control search import' placeholder='Найти сотрудника' />;
+};
+
+export default SearchPanel;
