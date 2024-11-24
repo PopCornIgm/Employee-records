8a9323eb3240c36e10400837a6efb2a515c37a45
PopCornIgm <popcornigorek.1993@gmail.com>
2024-11-24 13:47:16 +0700

    add props

--- my-app/src/components/employers-list/employers-list.js
+++ my-app/src/components/employers-list/employers-list.js
@@ -4,9 +4,9 @@ import './employers-list.css';
 const EmployersList = () => {
   return (
     <ul className='app-list list-group'>
-      <EmployersListItem />
-      <EmployersListItem />
-      <EmployersListItem />
+      <EmployersListItem name='Борис П.' salary={1000} />
+      <EmployersListItem name='Семен К.' salary={2000} />
+      <EmployersListItem name='Витас М.' salary={4000} />
     </ul>
   );
 };
